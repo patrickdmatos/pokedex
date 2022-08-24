79a6e012b99482660e99718f3a59625b5524ae69
patrickdmatos <patrick,1101reis@gmail.com>
2022-08-23 22:08:05 -0300

    pokedex

--- Pokedex/script.js
+++ Pokedex/script.js
@@ -1,14 +1,19 @@
 const pokemon_name = document.querySelector('.Pokemon_name');
 const pokemon_num = document.querySelector('.Pokemon_number');
 const pokemon_img = document.querySelector('.Pokemon_image');
+const buscapokemon = document.getElementById('numero_pokemon');
+const btnprev = document.getElementById('btn-prev');
+const btnnext = document.getElementById('btn-next');
+var achou = 1;
+buscapokemon.value = achou; 
 
 const fetchPokemon = async (pokemon) => {
-
     const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     
     const data = await APIresponse.json();
 
     return data;
+    
 }
 
 const renderPokemon = async (pokemon) => {
@@ -19,6 +24,9 @@ const renderPokemon = async (pokemon) => {
     pokemon_num.innerHTML = data.id;
     pokemon_img.src = data['sprites']['versions']['generation-v']['black-white']['animated']
     ['front_default'];
+    console.log(data)
 }
+renderPokemon(achou)
 
-renderPokemon('143')
+
+
