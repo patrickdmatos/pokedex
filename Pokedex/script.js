91b00ae5a9abc13f7f57c5a4e928ea181f2af1d7
patrickdmatos <patrick,1101reis@gmail.com>
2022-08-26 00:13:26 -0300

    pokedex finalizada

--- Pokedex/script.js
+++ Pokedex/script.js
@@ -4,19 +4,7 @@ const pokemon_img = document.querySelector('.Pokemon_image');
 const buscapokemon = document.getElementById('numero_pokemon');
 const btnprev = document.getElementById('btn-prev');
 const btnnext = document.getElementById('btn-next');
-
-    btnnext.addEventListener('click',{
-        decrement: function decrement(){
-            btnnext = document.getElementById('numero_pokemon');
-                btnnext = btnnext + 1;
-             buscapokemon.value = btnnext;
-        }
-    }); 
-function increment(){
-    span = document.getElementById('currentNumber');
-    currentNumber = currentNumber - 1;
-    span.innerHTML = currentNumber;
-};
+var retornapoke = 1;
 
 const fetchPokemon = async (pokemon) => {
     const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -35,14 +23,82 @@ const renderPokemon = async (pokemon) => {
     pokemon_num.innerHTML = data.id;
     pokemon_img.src = data['sprites']['versions']['generation-v']['black-white']['animated']
     ['front_default'];
-    buscapokemon.value = data.id;
-    btnnext.value = data.id;
-    btnprev.value = data.id;
-    console.log(data.id + 1)
 
 }
+renderPokemon(retornapoke)
 
-renderPokemon('1')
+    function aumenta(){
+            retornapoke = retornapoke + 1;
+             buscapokemon.value = retornapoke;
 
+             const fetchPokemon = async (pokemon) => {
+                const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+                
+                const data = await APIresponse.json();
+            
+                return data;
+                
+            }
+            
+            const renderPokemon = async (pokemon) => {
+            
+                const data = await fetchPokemon(pokemon);
+            
+                pokemon_name.innerHTML = data.name;
+                pokemon_num.innerHTML = data.id;
+                pokemon_img.src = data['sprites']['versions']['generation-v']['black-white']['animated']
+                ['front_default'];
+            
+            }
+            renderPokemon(retornapoke)
+         };
+    function diminui(){
+        retornapoke = retornapoke - 1;
+        buscapokemon.value = retornapoke;
 
+        const fetchPokemon = async (pokemon) => {
+            const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+            
+            const data = await APIresponse.json();
+        
+            return data;
+            
+        }
+        
+        const renderPokemon = async (pokemon) => {
+        
+            const data = await fetchPokemon(pokemon);
+        
+            pokemon_name.innerHTML = data.name;
+            pokemon_num.innerHTML = data.id;
+            pokemon_img.src = data['sprites']['versions']['generation-v']['black-white']['animated']
+            ['front_default'];
+            retornapoke = buscapokemon.value
+        }
+        renderPokemon(retornapoke)
+};
 
+function buscainput(){
+    retornapoke = buscapokemon.value;
+
+    const fetchPokemon = async (pokemon) => {
+        const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+        
+        const data = await APIresponse.json();
+    
+        return data;
+        
+    }
+    
+    const renderPokemon = async (pokemon) => {
+    
+        const data = await fetchPokemon(pokemon);
+    
+        pokemon_name.innerHTML = data.name;
+        pokemon_num.innerHTML = data.id;
+        pokemon_img.src = data['sprites']['versions']['generation-v']['black-white']['animated']
+        ['front_default'];
+        retornapoke = data.id;
+    }
+    renderPokemon(retornapoke)
+};
